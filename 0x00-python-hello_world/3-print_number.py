f3c35c59bdd113796d0b20ecaba841d8ca8d6284
gvibe3 <girmavibe@gmail.com>
2022-11-29 07:48:38 -0800

    fix

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,4 @@
 #!/bin/python3
 number = 98
-f"{number} Battery street"
+print(f"{number} Battery street")
+
--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(str * 3)
-Character = str[:9]
-print(character)
+print('{}'.format(str * 3))
+print('{:.9}'.format(str))
